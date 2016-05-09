b3851eb87108d63a69f52f26b20ea683a6d139a8
wddpeakking <wddpeakking@sina.com>
2016-05-09 15:01:25 +0800

    modify thread

--- lovelib/loveThread.cpp
+++ lovelib/loveThread.cpp
@@ -19,18 +19,51 @@ CLoveThread::~CLoveThread()
 {
 
 }
+CLoveThread::ThreadStatus CLoveThread::GetStatues()
+{
+	return m_statues;
+}
+
 void CLoveThread::trminate()
 {
-
+	if (m_statues == en_Running)
+	{
+		pthread_cancle(m_thread);
+		m_statues = en_Wait;
+	}
+	
 }
 void CLoveThread::Isrunning()
 {
-
+	return m_statues == en_Running;
 }
+
+void CLoveThread::wait()
+{
+	if (m_statues == en_Running)
+	{
+		int nRet = pthread_join(m_thread);
+		switch (nRet)
+		{
+		case 0:m_statues = en_Wait;break;
+		case EINVAL:		//thread is not join
+		case ESRCH:			// no thread found with this id
+		case EDEADLK:		//Deadlock detected or calling thread  waits for itself
+		default:			//unknow thread join error
+		}
+		if (m_statues != en_Terminate)
+		{
+			//thread did not finished 
+		}
+	}
+	
+}
+
 void CLoveThread::start()
 {
 	if (pthread_create(&m_thread,NULL,ProxyFun,this) != 0)
 	{
-
+		m_statues = en_UnKnow;
 	}
+	m_statues = en_Running;
 }
--- lovelib/loveThread.h
+++ lovelib/loveThread.h
@@ -4,14 +4,25 @@
 class CLoveThread
 {
 public:
+	enum ThreadStatus
+	{
+		en_UnKnow,
+		en_Running,
+		en_Terminate,
+		en_Wait
+	};
+
 	CLoveThread();
 	~CLoveThread();
 	
 	void start();
 	void trminate();
 	void Isrunning();
+	void wait();
+	CLoveThread::ThreadStatus GetStatues();
 	virtual void run()=0;
 private:
 	pthread_t m_thread;
+	ThreadStatus m_statues;
 £ý
 #endif
