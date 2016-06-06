e9ffee10aa5b00dce9675a500145a982187459f3
wddpeakking <wddpeakking@sina.com>
2016-06-06 17:05:10 +0800

    add tcp function

--- lovelib/lovetcp.cpp
+++ lovelib/lovetcp.cpp
@@ -1,7 +1,64 @@
 #include "lovetcp.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <sys/epoll.h>
+
 CLoveTcp::CLoveTcp()
 {
 }
 CLoveTcp::~CLoveTcp()
 {
+}
+void CLoveTcp::Init()
+{
+
+}
+
+void CLoveTcp::StartTcp(TCPType type,const std::string& strIp,int nPort)
+{
+	if((m_nSocket = socket(AF_INET,SOCK_STREAM,0))==-1)  
+	{
+		LogErrorMsg("create socket error");
+		exit(1);
+	}
+	//epoll Ä£ÐÍ
+	int nEpoll = epoll_create(256);
+	struct epoll_event ev,events[128];
+
+	struct sockaddr_in server_addr;
+	memset(&server_addr,0,sizeof(struct sockaddr_in));
+	server_addr.sin_family=AF_INET;
+	if (type == en_Server)
+	{
+		server_addr.sin_addr.s_addr=htonl(INADDR_ANY);
+		server_addr.sin_port=htons(nPort);
+		if(bind(m_nSocket,(struct sockaddr *)(&server_addr),sizeof(struct sockaddr))==-1) 
+		{ 
+			LogErrorMsg("bin socket error");
+			exit(1); 
+		} 
+		
+		if( listen( m_nSocket, MAXBACK ) == -1 )  
+		{  
+			LogErrorMsg("listen socket error");  
+			exit(1);  
+		}  
+		
+	}
+	else if (type == en_Client)
+	{
+		server_addr.sin_addr.s_addr=inet_addr(strIp.c_str());
+		server_addr.sin_port=htons(nPort);
+	}
+
+
 }
--- lovelib/lovetcp.h
+++ lovelib/lovetcp.h
@@ -1,12 +1,25 @@
 #ifndef _LoveChat_UDP_H_
 #define _LoveChat_UDP_H_
 /*  
-
+	tcp 提供消息的读取和发送
 */
+
 class CLoveTcp
 {
+	enum TCPType
+	{
+		en_Client,
+		en_Server,
+		en_Other
+	};
 public:
 	CLoveTcp();
 	~CLoveTcp();
+public:
+	void StartTcp(TCPType type=en_Server,const std::string& strIp="",int nPort=9999);
+private:
+	void Init(); 
+private:
+	int m_nSocket;
 };
 #endif
